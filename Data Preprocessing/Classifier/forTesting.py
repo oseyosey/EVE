e350c606f9fce7af2e10f56b0d6ed17abb4bd5d2
oscarscaro <32592746+oscarscaro@users.noreply.github.com>
2019-06-06 17:21:26 +0800

    Delete forTesting.py

--- Data Preprocessing/Classifier/forTesting.py
+++
@@ -1,9 +0,0 @@
-file=open("test.txt", mode="r", encoding="utf-8")
-while True:
-    line = file.readline()
-    if line:
-        print(line)
-        line_test;
-    else:
-        break
-file.close()
